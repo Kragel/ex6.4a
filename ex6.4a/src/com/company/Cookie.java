199cdb83551d44780828cf08cf7e0dcbfac6ed8e
Kragel <61646979+Kragel@users.noreply.github.com>
2020-03-09 15:44:02 +0300

    Add files via upload

---
+++ ex6.4a/src/com/company/Cookie.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Cookie {
+    public Cookie() {
+		System.out.println("Cookie contstructor");
+	}
+	protected void bite() { System.out.println("bite"); }
+}
---
+++ ex6.4a/src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Cookie x = new Cookie();
+        x.bite(); // package access to protected method
+    }
+}
